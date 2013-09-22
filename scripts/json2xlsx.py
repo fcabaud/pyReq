58585e72e29cbf8ff235ce868fac75406a8ccd39
fcabaud <fcabaud@gmail.com>
2013-09-22 09:29:13 +0200

    pep 8

--- scripts/json2xlsx.py
+++ scripts/json2xlsx.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # Goal : to extract dictionnary in an xlsx file from a list of requirements
 from pyReq import *
 from openpyxl import Workbook
 import argparse
 
-class getXlsx(pyReq):
-  def getXlsx(self, listOfReq = [], excelName = 'getReq.xlsx'):
+class ReqGetXlsx(pyReq):
+  """ class for export of requirements in excel 
+     :arg pyReq: json input requirements file 
+     :type pyReq: string
+  """
+  def get_xlsx(self, listOfReq = [], excelName = 'getReq.xlsx'):
+    """ Get xlslx file 
+    
+       :arg excelName: xlsx file containing requirements and attributes
+       :type excelName: string
+    """
     #dictReq = self.get(listOfReq)
     #print(listOfReq)
     if listOfReq == []:
@@ -54,7 +63,7 @@ class getXlsx(pyReq):
     print("Write xlsx file %s"%excelName)
 
 def test():
-  getReqInstance = getXlsx(C_PATH_WORK+"docExample.json")
+  getReqInstance = ReqGetXlsx(C_PATH_WORK+"docExample.json")
   #listOfTags = ['RQT_0001','RQT_0003']
   #print(getReqInstance.getKeys())
   # Example 1 : get all requirements not covered of sprint 2 : what validation team has to do
@@ -62,10 +71,10 @@ def test():
   for tag in listOfTagsSprint2:
     if getReqInstance[tag][C_KEY_COVERAGE] == []:
       print("%s Not covered"%tag)  
-  getReqInstance.getXlsx(listOfTagsSprint2, C_PATH_OUT+'reqListSprint2NotCovered.xlsx')  
+  getReqInstance.get_xlsx(listOfTagsSprint2, C_PATH_OUT+'reqListSprint2NotCovered.xlsx')  
   # Example 2 : get all requirements covered by a KO test : what development team has to do
   listOfTagsSprint1 = getReqInstance.getListReqFromAttribute("attributeStatus", "KO")
-  getReqInstance.getXlsx(listOfTagsSprint1, C_PATH_OUT+'reqListStatusKO.xlsx')  
+  getReqInstance.get_xlsx(listOfTagsSprint1, C_PATH_OUT+'reqListStatusKO.xlsx')  
     
 if __name__ == '__main__':
   #test()
@@ -76,6 +85,6 @@ if __name__ == '__main__':
   arguments = dict(result._get_kwargs())  
   #print(arguments['xlsxFileInput'])
   #print(arguments['jsonFileOutput'])
-  getReqInstance = getXlsx(arguments['jsonFileInput'])
+  getReqInstance = ReqGetXlsx(arguments['jsonFileInput'])
   listOfTagsSprint1 = getReqInstance.getListReqFromAttribute("attributeStatus", "KO")
-  getReqInstance.getXlsx(listOfTagsSprint1, arguments['xlsxFileOutput'])  
+  getReqInstance.get_xlsx(listOfTagsSprint1, arguments['xlsxFileOutput'])  
